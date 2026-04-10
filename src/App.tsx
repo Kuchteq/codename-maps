7d911321914e0ff6760fb4a284efb8a7602bbd18
Mariusz Kuchta <mariusz.kuchta@triona.fi>
2026-04-10 22:09:13 +0300

    MAP MVP

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Map from './components/Map';
+import Toolbar from './components/Toolbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Map />
+      <Toolbar />
+    </div>
+  );
+}
+
+export default App;
